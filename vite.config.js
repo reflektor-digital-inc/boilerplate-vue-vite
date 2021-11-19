3b147265af371f4ce4d63fe4a8324a3ad26372af
william <wltowxyz@gmail.com>
2021-11-19 12:41:20 -0500

    chore: add linting tools and sass setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins : [vue()],
+
+  // sass
+  css : { preprocessorOptions : { scss : { additionalData : '@import "src/styles/_shared.scss";', }, }, },
+
+  // import aliases
+  resolve : {
+    alias : {
+      '@/components' : path.resolve(__dirname, 'src/components'),
+      '@/pages'      : path.resolve(__dirname, 'src/pages'),
+      '@/assets'     : path.resolve(__dirname, 'src/assets'),
+      '@/styles'     : path.resolve(__dirname, 'src/styles'),
+      '@/settings'   : path.resolve(__dirname, 'src/settings'),
+      '@/utils'      : path.resolve(__dirname, 'src/utils'),
+      '@/store'      : path.resolve(__dirname, 'src/store'),
+      '@/hooks'      : path.resolve(__dirname, 'src/hooks'),
+      '@/canvas'     : path.resolve(__dirname, 'src/components/_canvas'),
+      '@/shaders'    : path.resolve(__dirname, 'src/shaders'),
+    },
+  },
+});
